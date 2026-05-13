b960bc1b6619520bb84f62a9f8d41eb9c3191fef
Faiza RABHI <93930498+Faizarabhi@users.noreply.github.com>
2026-05-13 20:28:14 +0100

    feat: initialize spring boot backend

---
+++ backend/src/main/java/com/faiza/invoicecrm/InvoicecrmApplication.java
@@ -0,0 +1,13 @@
+package com.faiza.invoicecrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicecrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicecrmApplication.class, args);
+	}
+
+}
